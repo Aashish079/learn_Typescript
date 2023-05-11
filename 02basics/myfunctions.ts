367b8f83d01b25fc08211fda00ba61ccb1591a2c
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2023-05-11 17:50:29 +0545

    type anotations in functions

---
+++ 02basics/myfunctions.ts
@@ -0,0 +1,28 @@
+function addTwo(num: number): number{
+    
+    return num + 2;
+}
+
+function getUpper(val: string){
+    return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+signUpUser("Aashish", "ak.dev", false);
+
+let logInUser = (name: string, email: string, isPaid: boolean =false)=>{}
+
+logInUser
+
+addTwo(5);
+getUpper("Aashish");
+
+function getValue(myVal: number): boolean | string{
+    if (myVal>5){
+        return true;
+    }
+    return "200 OK";
+}
+
+export{}
