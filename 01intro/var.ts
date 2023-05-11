4a93713b6002fc40b8068721ba1501be1de9216a
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2023-05-11 16:46:46 +0545

    Learned to apply type-checking in typescript

---
+++ 01intro/var.ts
@@ -0,0 +1,20 @@
+let greetings: string = "Hello";
+
+console.log(greetings);
+
+
+let userId = 33446.4
+userId.toFixed();
+
+let isLoggedIn: boolean =false;
+
+
+let hero: string;
+
+function getHero(){
+    return 'superman';
+}
+
+hero = getHero();
+
+export {};
